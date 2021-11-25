03f1259b2fe567e8f87d76a269eaa5dae551afd1
GitHub <noreply@github.com>
2021-11-25 00:18:50 +0000

    chore(deps): update deno dependencies

--- mod_test.ts
+++ mod_test.ts
@@ -1,4 +1,4 @@
-import { assertEquals } from 'https://deno.land/std@0.115.1/testing/asserts.ts';
+import { assertEquals } from 'https://deno.land/std@0.116.0/testing/asserts.ts';
 
 import randomJoke, {
 	allJokes,
