f20b9e194cf85289d9739b3e87a01dac1ab240fb
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-07-09 16:41:56 -0500

    :white_check_mark: Add Tests

---
+++ server/server_test.ts
@@ -0,0 +1,17 @@
+import { superoak } from "https://deno.land/x/superoak@4.7.0/mod.ts";
+
+import { equal } from "https://deno.land/std@0.135.0/testing/asserts.ts";
+
+import { app } from "./app.ts";
+import { jokes } from "./data.ts";
+
+Deno.test("Main", async () => {
+  const request = await superoak(app);
+
+  await request.get("/").expect(({ body }) => {
+    if (!jokes.find((obj) => equal(obj, body))) {
+      console.log(body);
+      throw new Error("Did not find this joke.");
+    }
+  });
+});
