1244888d3200577c2aefdd0347d6838b3f448549
GitHub <noreply@github.com>
2022-08-12 00:02:22 +0000

    chore(deps): update deno dependencies

--- server/server_test.ts
+++ server/server_test.ts
@@ -1,6 +1,6 @@
 import { superoak } from "https://deno.land/x/superoak@4.7.0/mod.ts";
 
-import { equal } from "https://deno.land/std@0.151.0/testing/asserts.ts";
+import { equal } from "https://deno.land/std@0.152.0/testing/asserts.ts";
 
 import { app } from "./app.ts";
 import { jokes } from "./data.ts";
