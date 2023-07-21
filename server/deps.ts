9446e54d801177c4a66da856d76182ec9e48c621
GitHub <noreply@github.com>
2023-07-21 00:02:39 +0000

    chore(deps): update deno dependencies

--- server/deps.ts
+++ server/deps.ts
@@ -3,6 +3,6 @@ export {
   type Middleware,
   Router,
   type RouterMiddleware,
-} from "https://deno.land/x/oak@v11.1.0/mod.ts";
+} from "https://deno.land/x/oak@v12.6.0/mod.ts";
 
 export { randomItem } from "https://deno.land/x/random_item@v1.2.0/mod.ts";
